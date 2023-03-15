73c90404074377f6f5be7e3ab9ad4be9953751d2
Andrey Codez <andreycodez@bixtrix.com>
2023-03-15 21:04:08 +0500

    Updated e3 with a new task of getStringSum

--- exercises/e3.js
+++ exercises/e3.js
@@ -1,13 +1,30 @@
 
 // EXERCISE 3
-// Return the average value for the given array of numbers, f.i.
-// getAverage([22, 45, 4, 65]) => 34
+// Make sure to solve two parts beneath
+
+/** 
+ * PART 1
+ * Return the average value for the given array of numbers.
+ * Example: getAverage([22, 45, 4, 65]) => 34
+ * */
 
 export function getAverage(array) {
   // Your code goes here...
 
 }
 
+
+/** 
+ * PART 2
+ * Create a getStringSum(str) function that returns the sum of any integers that are in the string.
+ * Example1: getStringSum("GH2U87A") => 17
+ * Example2: getStringSum("GHIUJUHSG") => 0
+ * */ 
+
+export function getStringSum(str) {
+  // Your code goes here...
+
+}
 
 
 // === TEST YOURSELF ===
